bbd9aca62c76920fd870d9b8e8943708b1c4f288
Martun <65818128+martun-avagyan@users.noreply.github.com>
2022-03-13 20:45:51 +0400

    feat: functionality to open and close modal by clicking on the backdrop and okay button

--- src/Components/UI/ErrorModal.js
+++ src/Components/UI/ErrorModal.js
@@ -5,17 +5,20 @@ import classes from "./ErrorModal.module.css";
 
 const ErrorModal = (props) => {
   return (
-    <Card className={classes.modal}>
-      <header className={classes.header}>
-        <h2>{props.title}</h2>
-      </header>
-      <div className={classes.content}>
-        <p>{props.message}</p>
-      </div>
-      <footer className={classes.actions}>
-        <Button>Okay</Button>
-      </footer>
-    </Card>
+    <div>
+      <div onClick={props.errorHandler} className={classes.backdrop} />
+      <Card className={classes.modal}>
+        <header className={classes.header}>
+          <h2>{props.title}</h2>
+        </header>
+        <div className={classes.content}>
+          <p>{props.message}</p>
+        </div>
+        <footer className={classes.actions}>
+          <Button onClick={props.errorHandler}>Okay</Button>
+        </footer>
+      </Card>
+    </div>
   );
 };
 
